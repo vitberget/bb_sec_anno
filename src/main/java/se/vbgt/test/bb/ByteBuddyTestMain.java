0ddd220fd1678e3db6cdc49163564b47fc3a1926
Kenneth Hedman <github@vitberget.se>
2020-02-18 18:30:40 +0100

    Check all loaded classes and add check

--- src/main/java/se/vbgt/test/bb/ByteBuddyTestMain.java
+++ src/main/java/se/vbgt/test/bb/ByteBuddyTestMain.java
@@ -6,6 +6,9 @@ import net.bytebuddy.asm.Advice;
 import net.bytebuddy.asm.AsmVisitorWrapper;
 import net.bytebuddy.dynamic.loading.ClassReloadingStrategy;
 
+import java.lang.reflect.Method;
+import java.util.Arrays;
+
 import static java.lang.System.out;
 import static net.bytebuddy.matcher.ElementMatchers.isAnnotatedWith;
 
@@ -17,19 +20,36 @@ public class ByteBuddyTestMain {
         out.printf("1 %s%n", foo.it("muh"));
         out.printf("1 %s%n", foo.that("bäh"));
 
-        new ByteBuddy()
-            .redefine(Foo.class)
-            .visit(createAnnoWrapper())
-            .make()
-            .load(Foo.class.getClassLoader(), ClassReloadingStrategy.fromInstalledAgent());
+        Arrays.stream(ByteBuddyAgent.getInstrumentation().getAllLoadedClasses())
+              .filter(c -> containsAnnoMethod(c))
+              .forEach(c -> buddyClass(c));
+
 
         out.printf("2 %s%n", foo.it("nem"));
         out.printf("2 %s%n", foo.that("nem"));
+    }
+
+    private static void buddyClass(Class c) {
+        new ByteBuddy()
+            .redefine(c)
+            .visit(createAnnoWrapper())
+            .make()
+            .load(c.getClassLoader(), ClassReloadingStrategy.fromInstalledAgent());
     }
 
     private static AsmVisitorWrapper createAnnoWrapper() {
         return Advice
             .to(AnnoInterceptor.class)
             .on(isAnnotatedWith(Anno.class));
+    }
+
+    private static boolean containsAnnoMethod(Class c) {
+        return Arrays.stream(c.getMethods())
+                     .anyMatch(m -> hasAnnoAnnotation(m));
+    }
+
+    private static boolean hasAnnoAnnotation(Method m) {
+        return Arrays.stream(m.getDeclaredAnnotations())
+              .anyMatch(a -> a instanceof Anno);
     }
 }
